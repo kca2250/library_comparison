384f1499fb38b1eb0e7109790e8f6277b7cadbb2
kca2250 <katok2250@gmail.com>
2022-01-12 22:38:39 +0900

    add: USERインターフェイスの作成とSampleCardコンポーネントに値を挿入

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -1,38 +1,57 @@
-import React, { useEffect, useState } from "react"
-import { instance } from "../apiClient"
+import React, { useEffect, useState } from "react";
+import { instance } from "../apiClient";
+import SampleCard from "./SampleCard";
 
 export interface USER {
-  id?: number
-  name: string
-  username: string
-  email: string
+  id?: number;
+  name: string;
+  username: string;
+  email: string;
   address: {
-    street: string
-    suite: string
-    city: string
-    zipcode: string
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
     geo: {
-      lat: string
-      lng: string
-    }
-  }
-  phone: string
-  website: string
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
   company: {
-    name: string
-    catchPhrase: string
-    bs: string
-  }
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
 
 const CardList: React.FC = () => {
-  const [users, setUsers] = useState<USER[]>([])
+  const [users, setUsers] = useState<USER[]>([]);
   useEffect(() => {
     instance.get("users").then((res) => {
-      setUsers(res.data)
-    })
-  }, [])
-  return <div>this is card list.</div>
-}
+      setUsers(res.data);
+    });
+  }, []);
+  return (
+    <>
+      {users.map((user: USER, index: number) => {
+        return (
+          <div style={{ marginBottom: "10px" }} key={index}>
+            <SampleCard
+              name={user.name}
+              username={user.username}
+              email={user.email}
+              phone={user.phone}
+              website={user.website}
+              company={user.company}
+              address={user.address}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-export default CardList
+export default CardList;
