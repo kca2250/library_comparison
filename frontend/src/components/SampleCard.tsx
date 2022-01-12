29d91ae6c54fe9a53ad568b2480e10517f2edb68
kca2250 <katok2250@gmail.com>
2022-01-12 22:39:38 +0900

    fix: レイアウトを編集しCollapseを追加

--- frontend/src/components/SampleCard.tsx
+++ frontend/src/components/SampleCard.tsx
@@ -1,58 +1,69 @@
-import React from "react"
-import { Avatar, Card, Col, Row, Tag, Typography } from "antd"
-import {
-  HeartOutlined,
-  ShareAltOutlined,
-  DownOutlined,
-} from "@ant-design/icons"
+import React from "react";
+import { Avatar, Card, Col, Collapse, Row, Tag, Typography } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import IconList from "./IconList";
+import { USER } from "./CardList";
 
-const { Text, Paragraph } = Typography
+const { Text, Paragraph } = Typography;
+const { Panel } = Collapse;
 
-const iconStyle: { fontSize: string } = {
-  fontSize: "1.2rem",
-}
-
-const SampleCard: React.FC = () => {
+const SampleCard: React.FC<USER> = (props) => {
+  const { name, username, email, phone, website, company, address } = props;
   return (
     <Card>
       <Row style={{ marginBottom: "8px" }} align="middle">
         <Col>
-          <Avatar size="large" />
+          <Avatar icon={<UserOutlined />} alt={username} size="large" />
         </Col>
         <Col style={{ marginLeft: "8px" }}>
           <Text strong>
-            name:<span style={{ color: "#686868" }}>username</span>
+            {name}:<span style={{ color: "#686868" }}>{username}</span>
           </Text>
           <br />
-          <Tag>email</Tag>
-          <Tag>phone</Tag>
+          <Tag>{email}</Tag>
+          <Tag>{phone}</Tag>
+          <Tag>{website}</Tag>
         </Col>
       </Row>
 
       <Paragraph>
         <Text style={{ marginBottom: "4px" }} strong>
-          company.name
+          {company.name}
         </Text>
         <br />
-        <Text>company.catchPhrase</Text>
+        <Text>{company.catchPhrase}</Text>
         <br />
-        <Text>company.bs</Text>
+        <Text>{company.bs}</Text>
         <br />
       </Paragraph>
 
-      <Row>
-        <Col span={1}>
-          <HeartOutlined style={iconStyle} />
-        </Col>
-        <Col span={22}>
-          <ShareAltOutlined style={iconStyle} />
-        </Col>
-        <Col span={1}>
-          <DownOutlined style={iconStyle} />
-        </Col>
-      </Row>
+      <Collapse
+        ghost
+        style={{ backgroundColor: "white" }}
+        expandIconPosition={"right"}
+      >
+        <Panel
+          style={{ backgroundColor: "white" }}
+          key={""}
+          header={<IconList />}
+        >
+          <Paragraph>
+            <Text strong>ADDRESS</Text>
+            <br />
+            <Tag>{address.zipcode}</Tag>
+            <br />
+            <Text>
+              {address.city} {address.street} {address.suite}
+            </Text>
+            <br />
+            <Text>
+              {address.geo.lat}: {address.geo.lng}
+            </Text>
+          </Paragraph>
+        </Panel>
+      </Collapse>
     </Card>
-  )
-}
+  );
+};
 
-export default SampleCard
+export default SampleCard;
