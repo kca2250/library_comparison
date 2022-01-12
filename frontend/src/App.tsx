2a0ce59c4207f99ff603072470d8ed5433789ae8
kca2250 <katok2250@gmail.com>
2022-01-12 22:37:54 +0900

    fix: レイアウトのスタイルの変更とCardListの表示

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,27 @@
-import React from "react"
-import { Layout } from "antd"
-import CardList from "./components/CardList"
-import SampleCard from "./components/SampleCard"
+import React from "react";
+import { Layout } from "antd";
+import CardList from "./components/CardList";
 
-const { Content, Footer, Header, Sider } = Layout
+const { Content, Footer, Header, Sider } = Layout;
 
 const bgColor: { backgroundColor: string } = {
   backgroundColor: "white",
-}
+};
 
 const App: React.FC = () => {
   return (
-    <Layout>
+    <Layout style={{ width: "1024px", margin: "auto" }}>
       <Header style={bgColor} />
       <Layout>
         <Sider breakpoint="sm" theme="light" />
         <Content style={bgColor}>
           <CardList />
-          <SampleCard />
         </Content>
         <Sider breakpoint="sm" theme="light" />
       </Layout>
       <Footer style={bgColor} />
     </Layout>
-  )
-}
+  );
+};
 
-export default App
+export default App;
